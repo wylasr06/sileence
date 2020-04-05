3e47808431fab956e2736dcaf08f369186818f8c
wylasr06 <1510720220@qq.com>
2020-04-05 10:55:00 +0800

    Ajax上传新闻内容及图片,以json作为数据交互格式，Postman测试Post请求

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -11,19 +11,20 @@ $(document).ready(
             $(".releaseHistory_div").css("margin-top","10px");
         });
 
-        $(".addPicture_div").click(function () {
+        /*$(".addPicture_div").click(function () {
             console.log("stupid fucking guy...")
             $(".issuePicture_div").show();
-        });
+        });*/
 
-        $(".releaseIssue_div").click(function () {
-            console.log($(".edit_panel_div").html()+" I am here with you");
-        });
+        // $(".releaseIssue_div").click(function () {
+        //     console.log($(".edit_panel_div").html()+" I am here with you");
+        // });
         /**文件上传和预览*/
         let readURL = function(input) {
             if (input.files && input.files[0]) {
                 let reader = new FileReader();
                 reader.onload = function (e) {
+                    $(".issuePicture_div").show();
                     $('.profile-pic').attr('src', e.target.result);
                     $('.profile-pic').css('display','block');
                 }
@@ -33,9 +34,46 @@ $(document).ready(
         $(".file-upload").on('change', function(){
             readURL(this);
         });
-        $(".upload-button").on('click', function() {
+        $(".addPicture_div").on('click', function() {
             $(".file-upload").click();
         });
         /**文件上传和预览*/
+
+        /**
+         * 发表新鲜事
+         */
+        $('.releaseIssue_div').on('click',function () {
+            let content = $(".edit_panel_div").html();
+            let img = $('.profile-pic')[0].src;
+            console.log("content : "+content+" , img : "+img);
+            /*$.post(
+                '/releaseNews',
+                {"content":content,"img":img},
+                function (result) {
+                console.log(result+" 我们都遍体鳞伤，也慢慢坏了心肠")
+                if(result.success >0){
+
+                }else{
+
+                }
+            },
+                "json");*/
+            $.ajax({
+                type:"post",
+                url:"/releaseNews",
+                contentType:"application/json",
+                dataType:"json",
+                data:JSON.stringify({"img":img,"content":content}),
+                success:function (result) {
+                    console.log("发表成功 : "+result);
+                },
+                error:function (msg) {
+                    console.log("发表新鲜事失败 "+msg);
+                }
+            })
+        })
+        /**
+         * 发表新鲜事
+         */
     }
 );
